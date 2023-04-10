42a230fbc5f2dd117af32337640a8ee75382dbce
Brandon <btoms.20@gmail.com>
2023-04-10 14:34:44 -0700

    Added explicit counter tests from Appendix A.5 from RFC9001 - v1 and v2

--- Tests/CryptoSwiftTests/ChaCha20Tests.swift
+++ Tests/CryptoSwiftTests/ChaCha20Tests.swift
@@ -106,6 +106,31 @@ final class ChaCha20Tests: XCTestCase {
       XCTFail()
     }
   }
+
+
+  /// Test Vector - https://datatracker.ietf.org/doc/html/rfc9001#name-chacha20-poly1305-short-hea
+  func testChaCha20ExplicitCounterV1() throws {
+    let hpKey = Array(hex: "0x25a282b9e82f06f21f488917a4fc8f1b73573685608597d0efcb076b0ab7a7a4")
+    /// Sample = 0x5e5cd55c41f69080575d7999c25a5bfb
+    let counter = Array(hex: "0x5e5cd55c")
+    let iv = Array(hex: "0x41f69080575d7999c25a5bfb")
+        
+    let mask = try CryptoSwift.ChaCha20(key: hpKey, counter: counter, iv: iv).encrypt(Array<UInt8>(repeating: 0, count: 5))
+
+    XCTAssertEqual(mask, Array(hex: "0xaefefe7d03"))
+  }
+
+  /// Test Vector - https://www.ietf.org/archive/id/draft-ietf-quic-v2-10.html#name-chacha20-poly1305-short-head
+  func testChaCha20ExplicitCounterV2() throws {
+    let hpKey = Array(hex: "0xd659760d2ba434a226fd37b35c69e2da8211d10c4f12538787d65645d5d1b8e2")
+    /// Sample = 0xe7b6b932bc27d786f4bc2bb20f2162ba
+    let counter = Array(hex: "0xe7b6b932")
+    let iv = Array(hex: "0xbc27d786f4bc2bb20f2162ba")
+      
+    let mask = try CryptoSwift.ChaCha20(key: hpKey, counter: counter, iv: iv).encrypt(Array<UInt8>(repeating: 0, count: 5))
+
+    XCTAssertEqual(mask, Array(hex: "0x97580e32bf"))
+  }
 }
 
 extension ChaCha20Tests {
